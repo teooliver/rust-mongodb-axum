9ba6640169b69a2fc605333581d3d6cc06f0bc3e
Teo Oliver <teo.liz.oli@gmail.com>
2021-10-10 23:40:45 +0100

    initial MongoDB rust driver config

--- src/controllers/tasks.rs
+++
@@ -1,3 +0,0 @@
-use crate::{db::DB, WebResult};
-
-pub fn get_task(db: Db) {}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use axum::{handler::get, Router};
+use mongodb::{
+    bson::{self, doc, Document},
+    Client, Collection,
+};
+use thiserror::Error;
 // Handler that immediately returns an empty `200 OK` response.
 async fn unit_handler() {}
 
@@ -8,8 +13,35 @@ async fn string_handler() -> String {
     "Hello, World!!!!!!".to_string()
 }
 
+// async fn create_task()->
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("mongodb error: {0}")]
+    MongoError(#[from] mongodb::error::Error),
+    #[error("error during mongodb query: {0}")]
+    MongoQueryError(mongodb::error::Error),
+    #[error("could not access field in document: {0}")]
+    MongoDataError(#[from] bson::document::ValueAccessError),
+    #[error("invalid id used: {0}")]
+    InvalidIDError(String),
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
+    let mut client = Client::with_uri_str("mongodb://127.0.0.1:27017").await?;
+    let time_tracker_base_db = client.database("time-tracker-base");
+
+    let tasks: Collection<Document> = time_tracker_base_db.collection("tasks");
+
+    tasks
+        .insert_one(
+            doc! {
+                "hello": "hello there"
+            },
+            None,
+        )
+        .await?;
     // build our application with a single route
     let app = Router::new().route("/", get(string_handler));
 
@@ -18,4 +50,6 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+    println!("Runnin on localhost:3000");
+    Ok(())
 }
